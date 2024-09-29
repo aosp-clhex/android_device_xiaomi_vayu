2c7a42f6266ce48c9a95b251954d86b6aa33114e
basamaryan <basam.aryan@gmail.com>
2024-09-29 16:24:14 -0400

    vayu: Move to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625

--- device.mk
+++ device.mk
@@ -175,7 +175,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     libkeymaster_messages.vendor
 
