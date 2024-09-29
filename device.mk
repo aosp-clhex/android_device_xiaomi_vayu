f39419df18a2ce876541e3400fb3664e9b85f738
Cyber Knight <cyberknight755@gmail.com>
2024-09-29 02:19:42 +0000

    vayu: Build libgui.vendor instead
    - AOSP 15 has made libgui vendor_available so just build libgui.vendor
      instead.
    
    Change-Id: I4bea2a3374ba18b87d08f153b614c4455f82b4d3

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libgui_vendor \
+    libgui.vendor \
     libpng.vendor \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor \
