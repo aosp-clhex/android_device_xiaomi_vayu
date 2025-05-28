adfb416484b341b76b9adfe07ae1d2108f838b23
grepfox <grepfox@tutamail.com>
2025-05-28 13:41:49 +0530

    sm8150-common: De-commonize XiaomiParts
    * since we are gonna import some device specific stuff to xiaomi parts
    
    Change-Id: I9596485f95f278b1f73b0a7bcf2c555417ceef8e

--- msmnile.mk
+++ msmnile.mk
@@ -145,10 +145,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
