c73223cedeec5501440c736b578ef59edd76dae5
clhex <100755123+clhexftw@users.noreply.github.com>
2025-07-03 07:37:52 +0000

    sm8150-common: Drop reduant xiaomi telephony stub

--- msmnile.mk
+++ msmnile.mk
@@ -335,12 +335,10 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext \
-    xiaomi-telephony-stub
+    telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext \
-    xiaomi-telephony-stub
+    telephony-ext
 
 # USB
 PRODUCT_PACKAGES += \
