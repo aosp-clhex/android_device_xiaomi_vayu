fb60bbe5fcaf757e4937350fb81f64ab0a0281c0
clhex <100755123+clhexftw@users.noreply.github.com>
2025-07-03 06:15:24 +0000

    sm8150-common: Use common usb component

--- msmnile.mk
+++ msmnile.mk
@@ -271,7 +271,8 @@ TARGET_BOARD_PLATFORM := msmnile
 # Components
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
-    perf
+    perf \
+    usb
 
 # Power
 PRODUCT_PACKAGES += \
@@ -300,14 +301,12 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
     init.qti.chg_policy.sh \
     init.qti.dcvs.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
